2844fc0fe31a944e56970904a55f4d9c31659561
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2024-11-06 00:15:10 +0100

    Fix accessibilità, fix SEO

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
         if(theme=="light")
         {
             $("#modeSelector").removeClass("btn-dark").addClass("btn-light");
-            $("#modeSelector").html("<img src='images/darkMode.svg'>");
+            $("#modeSelector").html("<img src='images/darkMode.svg' alt='Change color theme'>");
         }
         else
         {
             $("#modeSelector").removeClass("btn-light").addClass("btn-dark");
-            $("#modeSelector").html("<img src='images/lightMode.svg'>");
+            $("#modeSelector").html("<img src='images/lightMode.svg' alt='Change color theme'>");
         }
     }
     else
@@ -27,7 +27,7 @@ function changeTheme()
     {
         document.documentElement.setAttribute('data-bs-theme', 'dark');
         $("#modeSelector").removeClass("btn-light").addClass("btn-dark");
-        $("#modeSelector").html("<img src='images/lightMode.svg'>");
+        $("#modeSelector").html("<img src='images/lightMode.svg' alt='Change color theme'>");
         localStorage.setItem('theme', 'dark');
 
     }
@@ -35,7 +35,7 @@ function changeTheme()
     {
         document.documentElement.setAttribute('data-bs-theme', 'light');
         $("#modeSelector").removeClass("btn-dark").addClass("btn-light");
-        $("#modeSelector").html("<img src='images/darkMode.svg'>");
+        $("#modeSelector").html("<img src='images/darkMode.svg' alt='Change color theme'>");
         localStorage.setItem('theme', 'light');
     }
 }
